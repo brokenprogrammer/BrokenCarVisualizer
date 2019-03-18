0fc25c0ae368619df80105d6372e1e7bcb15dde7
Oskar Mendel <brokenprogrammer@gmail.com>
2019-03-18 15:19:30 +0100

    Removed old unused css styling.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         loader: 'style-loader!css-loader'
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|csv)$/,
         use: [
           'file-loader'
         ]
